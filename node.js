3879b244e3daff439c4b394ccdd83dcd7e631f04
mohanarajan1990 <99423268+mohanarajan1990@users.noreply.github.com>
2022-02-25 23:40:15 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,23 @@
+let obj = [
+    {name:"mohan",class:"phy",age:"25",bg:"a+"},
+    {name:"sharmila",class:"phy",age:"20",bg:"a+"},
+    {name:"devesh",class:"phy",age:"15",bg:"a+"},
+];
+obj.forEach(v => {
+    console.log(v.name);
+});
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
